63e78b3d5f7968627ea619b51106baf1e94fee14
Mauer-Oats <maueroats@gmail.com>
2025-10-21 12:38:02 -0500

    wy-cs site update 2025-10-21 12:38

--- js/main.js
+++ js/main.js
@@ -6410,7 +6410,7 @@ limitations under the License.
           }
 
           var query = $(this).val();
-          var searchPage = 'http://localhost:1313/search/?q=' + query;
+          var searchPage = 'http://localhost:36055/search/?q=' + query;
           document.location = searchPage;
 
           return false;
